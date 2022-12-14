b7e9b66800c95fd85e6eac9f59f54117acadae6a
Daniel Safronov <mog77a@gmail.com>
2022-12-13 21:49:34 -0700

    added local json database

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import asyncio
 import asyncpraw
 import discord
 import aiohttp
-import psycopg2
+import json
 import datetime
 import urllib.parse
 from discord.ext import commands
@@ -19,6 +19,21 @@ root = logging.getLogger()
 root.setLevel(logging.INFO)
 
 load_dotenv()
+database_file = "post_ids_database.json"
+
+
+def create_database(database_file):
+    '''Create a database to hold the post ids and timestamps'''
+    # Create a dictionary to hold the post_id and timestamp
+    posts = {}
+    # Create a json file to hold the dictionary
+    with open(database_file, 'w') as f:
+        json.dump(posts, f)
+
+
+# if the database doesn't exist, create it and add the default values
+if not (os.path.exists(database_file)):
+    create_database(database_file)
 
 
 # Add subreddits to check. Make sure the API calls don't exceed 1/sec
@@ -71,7 +86,7 @@ async def webhook_coroutine(post):
         discord_title = discord_title[0:100]
         parsed_title = post.title[0:100]
         parsed_title = urllib.parse.quote(parsed_title)
-        message = f"Hello!\nI am interested in buying delete_and_type_inquiry_here_dont_forget_title\n\nMy Paypal for an invoice request through [Paypal goods and services](https://www.paypal.com/myaccount/transfer/homepage/request) is:\n{PAYPAL_EMAIL}\n\n\nShipping address is handled through Paypal.\n\nThanks!"
+        message = f"Hello!\n\nI am interested in buying delete_and_type_inquiry_here_dont_forget_subject_and_PM\n\nMy Paypal for an invoice request through [Paypal goods and services](https://www.paypal.com/myaccount/transfer/homepage/request) is:\n{PAYPAL_EMAIL}\n\n\nShipping address is handled through Paypal.\n\nThanks!"
         message = urllib.parse.quote(message)
         icon_url = keyword_filter[sub]['icon']
 
@@ -86,6 +101,7 @@ async def webhook_coroutine(post):
         if len(description) > 4096:
             description = description[0:4096]
 
+        # messaage link handling
         if (sub in ['hardwareswap', 'appleswap']):
             embed = discord.Embed(title=discord_title, url=url,
                                   color=randColor, description=description, timestamp=datetime.datetime.utcnow())
@@ -100,7 +116,7 @@ async def webhook_coroutine(post):
         # print("[NEW POST]r/{0}: {1}\n".format(post.subreddit, post.title))
 
 
-async def reddit_channel(subreddits, reddit):
+async def reddit_channel(database_file, subreddits, reddit):
     channel = bot.get_channel(channelid)
 
     num_subreddits = 0
@@ -115,17 +131,17 @@ async def reddit_channel(subreddits, reddit):
         print(F"{datetime.datetime.now()}: Scraping new posts")
         posts = await ScrapePosts(s, reddit)
         # check the posts if a new post has been made and it matches the keywords
-        await check_posts(posts)
+        await check_posts(database_file, posts)
         print('\n')
 
         await asyncio.sleep(random.randint(len(subs)*3, len(subs)*4))
 
 
-async def check_posts(posts):
+async def check_posts(database_file, posts):
     # Look through posts and check if they contain the keywords
     try:
         print(F"{datetime.datetime.now()}: Checking for posts in database")
-        post_ids_exist = await database_multiple_post_check(posts)
+        post_ids_exist = await database_multiple_post_check(database_file, posts)
         for p in posts:
            # check if post exists in the database
             if post_ids_exist[p.id] is False:
@@ -155,90 +171,117 @@ async def check_posts(posts):
         print(e)
 
 
-async def database_post_check(post):
+async def database_post_check(database_file, post):
+    # Open the json file and load the dictionary
+    with open(database_file, 'r') as f:
+        posts = json.load(f)
 
-    ''' Checks if the post is already in the database, if not it will add it to the database and send a notification'''
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    # Creating a cursor (a DB cursor is an abstraction, meant for data set traversal)
-    cur = conn.cursor()
-    # Executing your PostgreSQL query
-    select_query = "SELECT EXISTS (SELECT 1 from redditpostalerts WHERE post_id = '" + str(
-        post.id) + "');"
-
-    cur.execute(select_query)
-
-    post_id = cur.fetchone()[0]
-
-    if post_id is False:
-
-        insert_query = "INSERT INTO redditpostalerts (post_id) VALUES ('" + str(
-            post.id) + "');"
-        cur.execute(insert_query)
-
-        delete_older_than_1_day_query = "DELETE FROM redditpostalerts WHERE timestamp < NOW() - INTERVAL '1 DAY';"
-        cur.execute(delete_older_than_1_day_query)
-
-        # In order to make the changes to the database permanent, we now commit our changes
-        conn.commit()
-
-    # We have committed the necessary changes and can now close out our connection
-    cur.close()
-    conn.close()
-    return post_id
+    # Check if the post id is in the dictionary
+    if post.id in posts:
+        return True
+    else:
+        # Add the post id and timestamp to the dictionary
+        posts[post.id] = str(datetime.datetime.now())
+        # Open the json file and dump the dictionary
+        with open(database_file, 'w') as f:
+            json.dump(posts, f)
+        return False
 
 
-async def database_multiple_post_check(posts):
-    ''' Checks if the multiple posts are in the database and returns a true/false value for each post
-        If false insert the post id into the database and remove any posts older than 1 day'''
+async def database_multiple_post_check(database_file, posts):
+    ''' Checks if the post is already in the database, if not it will add it to the database and return the ids that are not in the database'''
+    # Open the json file and load the dictionary
+    with open(database_file, 'r') as f:
+        posts_ids = json.load(f)
 
-    try:
-        # Dictionary to hold the post id and a boolean value if it exists in the database
-        post_ids_exist = {post.id: True for post in posts}
+    # Create a dictionary to hold the post ids that are not in the database
+    posts_in_database = {}
 
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        # Creating a cursor (a DB cursor is an abstraction, meant for data set traversal)
-        cur = conn.cursor()
+    # Loop through the posts and check if they are in the database
+    for p in posts:
+        if p.id in posts_ids:
+            posts_in_database[p.id] = True
+        else:
+            posts_in_database[p.id] = False
 
-        # values constructor
-        string2 = ", ".join(f"('{x}')" for x in post_ids_exist)
+    # Loop through the posts that are not in the database and add them to the database
+    for p in posts_in_database:
+        if posts_in_database[p] is False:
+            posts_ids[p] = str(datetime.datetime.now())
 
-        # Query that returns missing ids in database
-        select_query = f"(WITH v(id) as (VALUES{string2}) select v.id from v left join redditpostalerts i on i.post_id = v.id where i.post_id is null);"
+    # check for posts older than 1 day and remove them from the database
+    await delete_posts_older_than_1_day(posts_ids, database_file)
 
-        cur.execute(select_query)
-        missing_ids = cur.fetchall()
-
-        # update the database with missing ids
-        if missing_ids:
-            # unwrap the tuples in list
-            missing_ids = [x[0] for x in missing_ids]
-            # values constructor
-            string = ", ".join(f"('{x}')" for x in missing_ids)
-
-            # insert missing ids into database
-            insert_query = f"INSERT INTO redditpostalerts (post_id) VALUES {string};"
-            cur.execute(insert_query)
-
-            # set the missings id values to false
-            for id in missing_ids:
-                post_ids_exist[id] = False
-
-        # remove posts older than 1 day
-        delete_older_than_1_day_query = "DELETE FROM redditpostalerts WHERE timestamp < NOW() - INTERVAL '1 DAY';"
-        cur.execute(delete_older_than_1_day_query)
-
-        # In order to make the changes to the database permanent, we now commit our changes
-        conn.commit()
-    except Exception as e:
-        print(e)
-    finally:
+    return posts_in_database
 
 
-        # We have committed the necessary changes and can now close out our connection
-        cur.close()
-        conn.close()
+async def delete_posts_older_than_1_day(database, database_file):
+    # Open the json file and load the dictionary
 
-        return post_ids_exist
+    # Loop through the dictionary and remove any entries older than 1 day
+    for post in database.copy():
+        post_date = datetime.datetime.strptime(
+            database[post], '%Y-%m-%d %H:%M:%S.%f')
+        if (datetime.datetime.now() - post_date).days > 1:
+            del database[post]
+
+    # Open the json file and dump the dictionary
+    with open(database_file, 'w') as f:
+        json.dump(database, f)
+
+
+# async def database_multiple_post_check(posts):
+#     ''' Checks if the multiple posts are in the database and returns a true/false value for each post
+#         If false insert the post id into the database and remove any posts older than 1 day'''
+
+#     try:
+#         # Dictionary to hold the post id and a boolean value if it exists in the database
+#         post_ids_exist = {post.id: True for post in posts}
+
+#         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#         # Creating a cursor (a DB cursor is an abstraction, meant for data set traversal)
+#         cur = conn.cursor()
+
+#         # values constructor
+#         string2 = ", ".join(f"('{x}')" for x in post_ids_exist)
+
+#         # Query that returns missing ids in database
+#         select_query = f"(WITH v(id) as (VALUES{string2}) select v.id from v left join redditpostalerts i on i.post_id = v.id where i.post_id is null);"
+
+#         cur.execute(select_query)
+#         missing_ids = cur.fetchall()
+
+#         # update the database with missing ids
+#         if missing_ids:
+#             # unwrap the tuples in list
+#             missing_ids = [x[0] for x in missing_ids]
+#             # values constructor
+#             string = ", ".join(f"('{x}')" for x in missing_ids)
+
+#             # insert missing ids into database
+#             insert_query = f"INSERT INTO redditpostalerts (post_id) VALUES {string};"
+#             cur.execute(insert_query)
+
+#             # set the missings id values to false
+#             for id in missing_ids:
+#                 post_ids_exist[id] = False
+
+#         # remove posts older than 1 day
+#         delete_older_than_1_day_query = "DELETE FROM redditpostalerts WHERE timestamp < NOW() - INTERVAL '1 DAY';"
+#         cur.execute(delete_older_than_1_day_query)
+
+#         # In order to make the changes to the database permanent, we now commit our changes
+#         conn.commit()
+#     except Exception as e:
+#         print(e)
+#     finally:
+
+
+#         # We have committed the necessary changes and can now close out our connection
+#         cur.close()
+#         conn.close()
+
+#         return post_ids_exist
 
 
 async def ScrapePosts(subreddits, reddit, num_posts_toLoad=len(subs) * 1):
@@ -270,6 +313,7 @@ channel = bot.get_channel(channelid)
 async def on_ready():
     global reddit
     global subs
+    global database_file
     print(f'{bot.user} has connected to Discord!')
 
     channel = bot.get_channel(channelid)
@@ -280,7 +324,7 @@ async def on_ready():
         for channel in guild.channels:
 
             channel_array.append(channel)
-    await reddit_channel(subs, reddit)
+    await reddit_channel(database_file, subs, reddit)
 
 
 bot.run(token)
